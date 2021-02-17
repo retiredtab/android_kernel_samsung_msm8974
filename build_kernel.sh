71bc5777d544991e00049fe45bdcb3b0d2b61645
Valera1978 <valera1978@tut.by>
2021-02-17 15:12:27 +0300

    update script for build test kernel

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE=~/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 
+rm -rf output
 mkdir output
 
-make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -j64 -C $(pwd) O=output
-
-cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+#make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=lineageos_viennalteeur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output lineage_viennalteeur_defconfig
+make -j16 -C $(pwd) O=output
